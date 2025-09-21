212c5fb7d707ddc94eddabf51cc079550c9810ce
academicschoolbd <159193937+academicschoolbd@users.noreply.github.com>
2025-09-21 21:17:15 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,30 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('blog-cache').then(cache => {
-      return cache.addAll([
-        '/', 
-        '/index.html'
-      ]);
-    })
+const CACHE_NAME = "asblog-cache-v1";
+const urlsToCache = [
+  "/",            // Blogger root/home
+  "/favicon.ico"  // Favicon
+];
+
+// Install SW
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+// Fetch requests
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
     })
   );
 });
